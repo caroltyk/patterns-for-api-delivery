6794822df01e99b159d8e33ea41673b23de1a07d
caroltyk <carol@tyk.io>
2024-04-30 09:39:06 +0100

    folder name change

--- samples/change-management/api-changes.sh
+++ samples/change-management/api-changes.sh
@@ -1,1 +1,2 @@
 docker run --rm -t -v $(pwd):/apis tufin/oasdiff breaking /apis/redocly_openapi_3.0.json /apis/redocly_endpoint_deleted.json
+docker run --rm -t -v $(pwd):/apis tufin/oasdiff changelog /apis/redocly_openapi_3.0.json /apis/redocly_new_property.json
